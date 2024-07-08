1c6bd1ec336bf085bc0a2ab3fce8920b9a554ef1
Sagar Pingale <sagar.pingale@formeeholdings.com>
2024-07-08 08:09:10 +0000

    create insert record logic

--- app/Http/Controllers/IdentityController.php
+++ app/Http/Controllers/IdentityController.php
@@ -22,14 +22,7 @@ class IdentityController extends Controller
 
         if($exisiting_contact_count==0){
             // Create a new contact
-            $new_contact_id = DB::table('contacts')->insertGetId([
-                'phoneNumber' => $phone_number,
-                'email' => $email,
-                'linkPrecedence' => 'primary',
-                'linkedId' => null,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
+            $new_contact_id = $this->insert_record(NULL,$email,$phone_number,'primary');
 
             $new_contact = DB::table('contacts')->where('id', $new_contact_id)->first();
 
@@ -51,22 +44,64 @@ class IdentityController extends Controller
 
             if(!$matching_contact_by_email && $matching_contact_by_phone){
                 // Case when we get a match to a primary phone record
+
+                $this->insert_record($matching_contact_by_phone->id,$email,$phone_number,'secondary');
+                
+                return $this->coalesced_results();
             }
             else if(!$matching_contact_by_phone && $matching_contact_by_email){
                 // Case when we get a match to a primary email record
+
+                $this->insert_record($matching_contact_by_email->id,$email,$phone_number,'secondary');
+
+                return $this->coalesced_results();
             }
             else if($matching_contact_by_email && $matching_contact_by_phone && $matching_contact_by_email->id == $matching_contact_by_phone->id){
                 // Case when we get a match to both primary email and primary phone and id are also same
-                return response()->json(['error' => 'Orders were not placed due to duplicate identity'], 400);
+
+                // nothing to modify in database
+
+                return $this->duplicate_order();
             }
             else if($matching_contact_by_email && $matching_contact_by_phone && $matching_contact_by_email->id != $matching_contact_by_phone->id){
                 // Case when we get a match to both primary email and primary phone and id are different
+
+                $this->update_record($matching_contact_by_email,$matching_contact_by_phone);
+
+                return $this->coalesced_results();
             }
             else{
                 // Case when we get a match with a secondary record with either id or phone
+
+                return $this->coalesced_results();
             }
 
         }
+
+    }
+
+    public function duplicate_order(){
+        return response()->json(['error' => 'Orders were not placed due to duplicate identity'], 400);
+    }
+
+    public function coalesced_results(){
+
+    }
+
+    public function insert_record($linked_id,$email,$phone_number,$link_precedence){
+        $new_contact_id = DB::table('contacts')->insertGetId([
+            'phoneNumber' => $phone_number,
+            'email' => $email,
+            'linkPrecedence' => $link_precedence,
+            'linkedId' => $linked_id,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+
+        return $new_contact_id;
+    }
+
+    public function update_record($email_record,$phone_record){
 
     }
 }

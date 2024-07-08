2fa0acaf475a97c00a4f7089c4a98154e9166319
Sagar Pingale <sagar.pingale@formeeholdings.com>
2024-07-08 08:24:20 +0000

    add output logic

--- app/Http/Controllers/IdentityController.php
+++ app/Http/Controllers/IdentityController.php
@@ -27,7 +27,7 @@ class IdentityController extends Controller
             $new_contact = DB::table('contacts')->where('id', $new_contact_id)->first();
 
             // return the single new record
-            return response()->json($new_contact);
+            return $this->coalesced_results($new_contact);
         }
         else{
             // Get the primary email matching record for the given request email
@@ -47,14 +47,14 @@ class IdentityController extends Controller
 
                 $this->insert_record($matching_contact_by_phone->id,$email,$phone_number,'secondary');
                 
-                return $this->coalesced_results();
+                return $this->coalesced_results($matching_contact_by_phone);
             }
             else if(!$matching_contact_by_phone && $matching_contact_by_email){
                 // Case when we get a match to a primary email record
 
                 $this->insert_record($matching_contact_by_email->id,$email,$phone_number,'secondary');
 
-                return $this->coalesced_results();
+                return $this->coalesced_results($matching_contact_by_email);
             }
             else if($matching_contact_by_email && $matching_contact_by_phone && $matching_contact_by_email->id == $matching_contact_by_phone->id){
                 // Case when we get a match to both primary email and primary phone and id are also same
@@ -68,12 +68,12 @@ class IdentityController extends Controller
 
                 $this->update_record($matching_contact_by_email,$matching_contact_by_phone);
 
-                return $this->coalesced_results();
+                return $this->coalesced_results($matching_contact_by_email);
             }
             else{
                 // Case when we get a match with a secondary record with either id or phone
 
-                return $this->coalesced_results();
+                
             }
 
         }
@@ -84,8 +84,27 @@ class IdentityController extends Controller
         return response()->json(['error' => 'Orders were not placed due to duplicate identity'], 400);
     }
 
-    public function coalesced_results(){
+    public function coalesced_results($primary_record){
+        $linked_contacts = DB::table('contacts')->where('linkedId', $primary_record->id)->get();
 
+        $emails = [$primary_record->email];
+        $phoneNumbers = [$primary_record->phoneNumber];
+        $secondaryContactIds = [];
+
+        foreach ($linked_contacts as $contact) {
+            $emails[] = $contact->email;
+            $phoneNumbers[] = $contact->phoneNumber;
+            $secondaryContactIds[] = $contact->id;
+        }
+
+        return response()->json([
+            'contact' => [
+                'primaryContactId' => $primary_id,
+                'emails' => $emails,
+                'phoneNumbers' => $phoneNumbers,
+                'secondaryContactIds' => $secondaryContactIds,
+            ]
+        ]);
     }
 
     public function insert_record($linked_id,$email,$phone_number,$link_precedence){
@@ -102,6 +121,12 @@ class IdentityController extends Controller
     }
 
     public function update_record($email_record,$phone_record){
-
+        DB::table('contacts')
+        ->where('id', $phone_record->id)
+        ->update([
+            'linkPrecedence' => 'secondary',
+            'linkedId' => $email_record->id,
+            'updated_at' => now(),
+        ]);
     }
 }
